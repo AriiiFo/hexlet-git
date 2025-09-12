6567057d942eb54c61864d6f8bea911163175963
AriiFo <a218342@mail.ru>
2025-09-12 12:36:39 +0700

    feat: changes for v0.0.1

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-  greetingv1 "./greeting"
-  greetingv2 "./greeting/v2"
-  "fmt"
+	// greetingv1 "./greeting"
+	// greetingv2 "./greeting/v2"
+	// "fmt"
+	"github.com/fatih/color"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-  fmt.Println("Первое приветствие: ", greetingv1.Get(),
-	"\nВторое приветствие: ", greetingv2.Get(), 
-	"\nРезультат hello.go:", greetingv1.Hello())
+	//fmt.Println("Первое приветствие: ", greetingv1.Get(),
+	//	"\nВторое приветствие: ", greetingv2.Get(),
+	//	"\nРезультат hello.go: ", greetingv1.Hello())
+	logrus.Info("Hello, Hexlet!")
+	color.Cyan("Prints text in cyan.")
 }
